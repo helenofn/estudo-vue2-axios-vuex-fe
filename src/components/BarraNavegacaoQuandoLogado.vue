e8522041ef0545aa260c25e905181b9b7316783a
Heleno Freitas Neto <heleno.freitas@gmail.com>
2021-09-22 09:27:28 -0300

    first commit

---
+++ src/components/BarraNavegacaoQuandoLogado.vue
@@ -0,0 +1,30 @@
+<template>
+<ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+        <router-link class="nav-link" to="/">Home</router-link>
+    </li>
+    <li class="nav-item">
+        <router-link to="/gerentes" class="nav-link">
+            Gerentes
+        </router-link>
+    </li>
+    <li class="nav-item">
+        <a href="#" class="nav-link" @click.prevent="efetuarLogout">Logout</a>
+    </li>
+</ul>
+</template>
+
+<script>
+import { logoutMixin } from '@/mixins'
+
+export default{
+    /*methods:{
+        efetuarLogout(){
+            this.$store.commit('DESLOGAR_USUARIO');
+            //localStorage.removeItem('token');
+            this.$router.push({name:'login'});
+        }
+    }*/
+    mixins: [logoutMixin]
+}
+</script>
